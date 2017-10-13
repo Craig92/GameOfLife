1a284a9c860286f7e037cf4407aa79208d02a198
Thorsten F. <1431708@stud.hs-mannheim.de>
2017-10-13 17:02:04 +0200

    Klasse angelegt

---
+++ GameOfLive/src/main/Spielbrett.java
@@ -0,0 +1,46 @@
+package main;
+
+public class Spielbrett {
+
+	boolean array[][] = null;
+
+	private Spielbrett(int x, int y) {
+		array = new boolean[x][y];
+	}
+
+	boolean[] erstellen(int x, int y) {
+		return null;
+	}
+
+	void set(int x, int y) {
+
+	}
+
+	void reset() {
+
+	}
+
+	void random() {
+
+	}
+
+	void speichern(String filename) {
+
+	}
+
+	void laden(String filename) {
+
+	}
+
+	void start() {
+
+	}
+
+	void stop() {
+
+	}
+
+	void einzelSchritt() {
+
+	}
+}
