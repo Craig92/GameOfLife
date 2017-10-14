6af03388bbec4dd12e907993230ec3e6126de3e1
Lucas <lucas_kneis@yahoo.de>
2017-10-14 11:33:06 +0200

    Logik hinzugefügt

--- GameOfLive/src/main/Main.java
+++ GameOfLive/src/main/Main.java
@@ -2,4 +2,29 @@ package main;
 
 public class Main {
 
+    public static void main (String[] args){
+
+        int Länge;
+        int Breite;
+
+        System.out.println("Bitte geben Sie die Spielbrettgröße an");
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+
+        System.out.println("Länge angeben");
+        Länge = scanner.nextInt();
+
+        System.out.println("Breite angeben");
+        Breite = scanner.nextInt();
+
+        Spielbrett brett1 = new Spielbrett(Länge, Breite);
+
+      //  System.out.println("Spielbrett hat Länge" + brett1.array[].length + " Breite:" + brett1.array[].length  );
+
+
+
+
+    }
+
 }
--- GameOfLive/src/main/Spielbrett.java
+++ GameOfLive/src/main/Spielbrett.java
@@ -4,15 +4,29 @@ public class Spielbrett {
 
 	boolean array[][] = null;
 
-	private Spielbrett(int x, int y) {
+	public Spielbrett(int x, int y) {
+
 		array = new boolean[x][y];
+
+		for (int i = 0; i > array.length; i++){
+
+			for (int j = 0; j > array[i].length; j++ ){
+				array[i][j] = false;
+
+			}
+		}
+
 	}
 
-	boolean[] erstellen(int x, int y) {
-		return null;
-	}
 
-	void set(int x, int y) {
+	void set(int x, int y, boolean b) {
+
+		if (b){
+			array[x][y] = true;
+		}
+		else {
+			array[x][y] = false;
+		}
 
 	}
 
@@ -38,6 +52,31 @@ public class Spielbrett {
 
 	void stop() {
 
+	}
+
+	int checkNeighbours(int x, int y){
+
+		for (int i = 0; i > array.length; i++) {
+
+			for (int j = 0; j > array[i].length; j++) {
+				Boolean aktuell= array[i][j];
+
+				Boolean rechterNachbar = array[i+1][j+1];
+				Boolean rechtsUnten = array[i+1][j];
+				Boolean rechtsOben = array[i+1][j-1];
+
+				Boolean linkerNachbar = array[1-1][j+1];
+				Boolean linksOben = array[i-1][j];
+				Boolean linksUnten = array[i-1][j-1];
+
+				Boolean unten = array[i+1][j];
+				Boolean oben = array[i+1][j];
+
+
+			}
+
+		}
+		return 1;
 	}
 
 	void einzelSchritt() {
